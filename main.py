76cbabfc2829adaafc3dff476b70f66ba6d6181d
EmpathyZ <z.empathy.z@gmail.com>
2020-03-01 15:13:34 +0300

    Added no crash mode and compiled file

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3.8
-# -*- coding: utf-8 -*-
-
 import os
 import random
 import requests
@@ -11,34 +8,42 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:61.0) Gecko/20100101 Firefox/72.0'
 }
 
-def parse_audio():
-    while True:
-        global ebobocore
-        ebobocore = 1
-        num = '6' + str(random.randrange(1111, 9999))
-        num2 = str(random.randrange(111, 999)) + ".wav"
-        pizdec = num + num2
-        url = "http://static.donationalerts.ru/audiodonations/" + num + '/' + num + num2
-        r = requests.get(url, headers=headers)
-        if r.status_code == 200:
-            aaa(url, ebobocore)
-        else:
-            return parse_audio()
+def parse_audio():    
+        while True:
+            global ebobocore
+            ebobocore = 1
+            num = '6' + str(random.randrange(1111, 9999))
+            num2 = str(random.randrange(111, 999)) + ".wav"
+            url = "http://static.donationalerts.ru/audiodonations/" + num + '/' + num + num2
+            r = requests.get(url, headers=headers)
+            if r.status_code == 200:
+                aaa(url, ebobocore)
+            else:
+                return parse_audio()
 
-def aaa(url, ebobocore):
-    if os.path.exists('audio/' + str(ebobocore) + "_EboboCore.wav"):
+def aaa(url, ebobocore):    
+    if os.path.exists('audio/' + str(ebobocore) + "_EboboCore.wav"):   
         ebobocore += 1
-        return aaa(url, ebobocore)
+        return aaa(url, ebobocore)    
     else:
         print(str(ebobocore) + " voice donations downloaded")
         path = os.path.join('audio', f'{str(ebobocore) + "_EboboCore.wav"}')
         with open(path, 'wb') as f:
             f.write(requests.get(url, headers=headers).content)
 
+while True:
+        try:
+            if __name__ in '__main__':
+                if os.path.exists('audio'):
+                    print('CRASHED OR STARTED')
+                    parse_audio()
+                else:
+                    os.mkdir('audio')
+                    parse_audio()
 
-if __name__ in '__main__':
-    if os.path.exists('audio'):
-        parse_audio()
-    else:
-        os.mkdir('audio')
-        parse_audio()
+        except Exception:
+            os.system('python "main.py"')
+            print('CRASHED')
+            quit()
+
+
